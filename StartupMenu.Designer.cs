d0111c1f085e4236a18e873f766629bfe19b971b
iTschetter <isaiah.tschetter@trojans.dsu.edu>
2024-04-09 17:31:31 -0500

    Successfully implemented the eat and grow feature for snake.

---
+++ StartupMenu.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Snake_
+{
+    partial class StartupMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("ROG Fonts", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(970, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(660, 144);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Snake+";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("ROG Fonts", 15.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(1005, 460);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(566, 65);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Single Player";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // StartupMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(2598, 1365);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "StartupMenu";
+            this.Text = "StartupMenu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ StartupMenu.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake_
+{
+    public partial class StartupMenu : Form
+    {
+        Form1 gameForm;
+        public StartupMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            gameForm = new Form1();
+            gameForm.Show(this);
+            this.Hide();
+        }
+    }
+}
